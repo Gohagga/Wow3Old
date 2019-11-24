956cd776b94fdfced5b071f7c1aee7bdf63a9a2f
Bartul <bgrbin@tvz.hr>
2019-11-24 21:11:56 +0100

    add map data Config

---
+++ src-ts/Config.ts
@@ -0,0 +1,75 @@
+class Units {
+    static BloodMage            = FourCC("Hblm");
+}
+
+class Spells {
+    static WildGrowth           = FourCC("A999");
+    static Nourish              = FourCC("A999");
+    static Rejuvenation         = FourCC("A999");
+    static Swiftmend            = FourCC("A999");
+    static ChaosBolt            = FourCC("A999");
+    static Combustion           = FourCC("A999");
+    static Conflagrate          = FourCC("A999");
+    static CreateHealthstone    = FourCC("A999");
+    static CurseOfTheElements   = FourCC("A999");
+    static EntanglingRoots      = FourCC("A999");
+    static FireBlast            = FourCC("A999");
+    static Fireball             = FourCC("A999");
+    static Flamestrike          = FourCC("Ahfs");
+    static Immolate             = FourCC("A999");
+    static Incinerate           = FourCC("A999");
+    static Ironbark             = FourCC("A999");
+    static Pyroblast            = FourCC("A999");
+    static Scorch               = FourCC("A999");
+    static Shadowfury           = FourCC("A999");
+    static SoulFire             = FourCC("A999");
+    static SummonFelguard       = FourCC("A999");
+    static SummonImp            = FourCC("A999");
+    static Tranquility          = FourCC("A999");
+    static Revitalize           = FourCC("A999");
+}
+
+let path = "ReplaceableTextures\\CommandButtons\\"
+class Icons {
+
+    static Regeneration             = path + "BTNRegenerate.blp";
+    static Rejuvenation             = path + "BTNRejuvenation.blp";
+    static Butterflies              = path + "BTNANA_HealingButterfliesFixed.BLP";
+    static GreenBearPaw             = path + "BTNBear-Paw-Blue.blp";
+    static Revitalize               = path + "BTNReplenish.blp";
+    static MarkOfTheWild            = path + "BTNSpell_Nature_Regeneration.blp";
+    static Regrowth                 = path + "BTNSpell_Nature_ResistNature.blp";
+    static HealingTouch             = path + "BTNSpell_Nature_HealingTouch.blp";
+    static Furor                    = path + "BTNSpell_Nature_UnyeildingStamina.blp";
+    static Nourish                  = path + "BTNability_druid_nourish.blp";
+    static SwiftRejuvenation        = path + "BTNability_druid_empoweredrejuvination.blp";
+    static EmpoweredTouch           = path + "BTNability_druid_empoweredtouch.blp";
+    static Swiftmend                = path + "BTNinv_relics_idolofrejuvenation.blp";
+    static Efflorescence            = path + "BTNinv_misc_herb_talandrasrose.blp";
+    static WildGrowth               = path + "BTNNatureHealingRay.blp";
+    static NaturesMajesty           = path + "BTNAdvancedStrengthOfTheMoon.blp";
+    static Toughness                = path + "BTNspell_holy_devotion.blp";
+    static CrusaderStrike           = path + "BTNspell_holy_crusaderstrike.blp";
+    static Judgement                = path + "BTNHolyBash.blp";
+    static WordOfGlory              = path + "BTNHeal.blp";
+    static Vindication              = path + "BTNspell_holy_vindication.blp";
+    static AvengersShield           = path + "BTNspell_holy_avengersshield.blp";
+    static EternalGlory             = path + "BTNHealingWave.blp";
+    static Divinity                 = path + "BTNspell_holy_blindingheal.blp";
+    static JudgementsOfTheWise      = path + "BTNSorceressMaster.blp";
+    static TouchedByTheLight        = path + "BTNability_paladin_touchedbylight.blp";
+    static DivineProtection         = path + "BTNHumanArmorUpThree.blp";
+    static DevotionAura             = path + "BTNDevotion.blp";
+    static JudgementArmor           = path + "BTNHoly Paladin.blp";
+    static FireBlast                = path + "BTNspell_fire_fireball.blp";
+    static Scorch                   = path + "BTNSoulBurn.blp";
+    static Pyroblast                = path + "BTNSpell_Fire_Fireball02.blp";
+    static Firestarter              = path + "BTNspell_fire_playingwithfire.blp";
+    static ImprovedScorch           = path + "BTNSoulBurn.blp";
+    static Ignite                   = path + "BTNIncinerate.blp";
+    static Wildfire                 = path + "BTNability_mage_worldinflames.blp";
+    static HotStreak                = path + "BTNability_mage_hotstreak.blp";
+    static Impact                   = path + "BTNWallOfFire.blp";
+    static CriticalMass             = path + "BTNability_mage_firestarter.blp";
+    static Incinerate               = path + "BTNIncinerateWoW.blp";
+}
